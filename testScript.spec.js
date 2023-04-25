a2402b428ccfc5bfee3a405b176453905694b88e
Sardor Madaminov <sardor0968@gmail.com>
2023-04-25 19:42:06 +0900

    minor update

--- testScript.spec.js
+++ testScript.spec.js
@@ -1,6 +1,6 @@
 const { By, Browser } = require("selenium-webdriver");
-const { suite } = require("selenium-webdriver/testing");
 const assert = require("assert");
+const { suite } = require("selenium-webdriver/testing");
 
 suite(
   function (env) {
@@ -9,36 +9,35 @@ suite(
       before(async function () {
         driver = await new env.Builder().forBrowser("chrome").build();
       });
-      it(
-        ("Google Search ",
-        async function () {
-          await driver.get("https://www.google.com");
-
-          let title = await driver.getTitle();
-          assert.equal("Web Driver Test", title);
-
-          await driver.manager().setTimeouts({ implicit: 10000 });
-
-          let textBox = await driver.findElement(By.name("step-current"));
-          let submitButton = await driver.findElement(
-            By.css('button[type="submit"]')
-          );
-
-          await textBox.sendKeys("Google Search");
-          await submitButton.click();
-
-          let message = await driver.findElement(By.id("message"));
-          let value = await message.getText();
-          assert.equal("Google Search", value);
-        })
-      );
       after(async function () {
         await driver.quit();
+      });
+      it("Google Search ", async function () {
+        await driver.get("https://www.google.com");
+
+        let title = await driver.getTitle();
+        assert.equal("Web Driver Test", title);
+
+        await driver.manager().setTimeouts({ implicit: 10000 });
+
+        let textBox = await driver.findElement(By.name("step-current"));
+        let submitButton = await driver.findElement(
+          By.css('button[type="submit"]')
+        );
+
+        await textBox.sendKeys("Google Search");
+        await submitButton.click();
+
+        let logs = await driver.manage().logs().get("browser");
+        console.log(logs);
+
+        let message = await driver.findElement(By.id("message"));
+        let value = await message.getText();
+        assert.equal("Google Search", value);
       });
     });
   },
   { browsers: [Browser.CHROME, Browser.FIREFOX] }
 );
 
-// module.exports = describe;
 // // Path: testScript.spec.js
